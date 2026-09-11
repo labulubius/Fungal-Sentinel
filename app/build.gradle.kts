dcf20a62412877d76cd3777c5b627c1018b20501
labulubius <labulubius@gmail.com>
2026-09-11 16:58:58 +0800

    build: prepare v1.2.1 narrow-screen test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "org.fungalsentinel.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
