cf624e1efc9b43ba0335a5f0db3ee8c6e2954d3f
labulubius <labulubius@gmail.com>
2026-09-11 16:23:52 +0800

    build: prepare v1.2 device test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "org.fungalsentinel.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
